bddb4316decd883b85acd23ff0c052055f6a7666
elastic <elastic.code@gmail.com>
2018-07-29 10:27:17 +0200

    cleanup

--- index.js
+++ index.js
@@ -3,27 +3,32 @@
   if (typeof define === 'function' && define.amd) {
     // AMD
     define([
+      'web3',
       './digioptions_markets_abi',
       './digioptions_markets_bin'
     ], factory);
   } else if (typeof module !== 'undefined' && module.exports) {
     // CommonJS (node and other environments that support module.exports)
     module.exports = factory(
+      require('web3'),
       require('./digioptions_markets_abi'),
       require('./digioptions_markets_bin')
     );
   }else {
     // Global (browser)
     root.digioptionsContracts = factory(
+      root.Web3, // we expect that the whole Web3 was loaded an use only the utils from it
       root.digioptions_markets_abi,
       root.digioptions_markets_bin
     );
   }
-}(this, function (digioptions_markets_abi, digioptions_markets_bin) {
+}(this, function (Web3, digioptions_markets_abi, digioptions_markets_bin) {
+  var web3_utils = Web3.utils;
+
   return {
     digioptions_markets_abi: digioptions_markets_abi,
     digioptions_markets_bin: digioptions_markets_bin,
-    orderToHash: function(web3_utils, order){
+    orderToHash: function(order){
       return web3_utils.soliditySha3(
         {t: 'address', v: order.marketsAddr},
         {t: 'bytes32', v: order.marketFactHash},
