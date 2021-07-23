459599fb072cc6c0ace5b9c1f5eafa474b09b036
elastic <elastic.code@gmail.com>
2021-07-23 08:48:00 +0200

    check agains newer solc versions

--- tools/compile.js
+++ tools/compile.js
@@ -14,6 +14,13 @@ const optionsDefault = {
   // see https://solc-bin.ethereum.org/bin/list.json
   //solcVersion: '0.7.4+commit.3f05b770',
   solcVersion: '0.7.6+commit.7338295f',
+  //solcVersion: '0.8.0+commit.c7dfd78e',
+  //solcVersion: '0.8.1+commit.df193b15',
+  //solcVersion: '0.8.2+commit.661d1103',
+  //solcVersion: '0.8.3+commit.8d00100c',
+  //solcVersion: '0.8.4+commit.c7e474f2',
+  //solcVersion: '0.8.5+commit.a4f2e591',
+  //solcVersion: '0.8.6+commit.11564f7e',
   compilerSettings: {
     //evmVersion: 'byzantium', // for thundercore (as of 2020-05)
     //remappings: [
